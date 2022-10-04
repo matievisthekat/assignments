bc5ae1f8dde2f638e39641cc735d12e824b291ee
matievisthekat <matievisthekat@gmail.com>
2022-10-04 15:11:24 +0200

    add comments

--- car_class/main.cpp
+++ car_class/main.cpp
@@ -27,10 +27,12 @@ class Car {
       return make;
     }
 
+    // increase speed by 10
     void accelerate() {
       speed += 10;
     }
 
+    // decrease speed by 10
     void brake() {
       speed -= 10;
     }
@@ -39,14 +41,17 @@ class Car {
 int main() {
   Car myCar = { 2006, "ryder" };
 
+  // call Car.accelerate() 5 times and print the speed after each call
   cout << "accelerating..." << endl;
   for (int i = 0; i < 5; i++) {
     myCar.accelerate();
     cout << "speed: " << myCar.getSpeed() << endl;
   }
 
+  // just for formatting
   cout << endl;
 
+  // call Car.brake() 5 times and print the speed after each call
   cout << "braking..." << endl;
   for (int i = 0; i < 5; i++) {
     myCar.brake();
