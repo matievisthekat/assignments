fb2f64cb1fb8786b4667f45e048da33440456909
matievisthekat <matievisthekat@gmail.com>
2022-12-13 09:22:41 +0200

    add savings account class

---
+++ final/SavingsAccount.cpp
@@ -0,0 +1,33 @@
+#include "BankAccount.cpp"
+
+class SavingsAccount : public BankAccount {
+  bool status = true;
+
+  public:
+    void withdraw(float amt) {
+      if (status) {
+        BankAccount::withdraw(amt);
+      }
+    }
+
+    void deposit(float amt) {
+      if (!status && (balance + amt) > 25) {
+        status = true;
+      }
+
+      BankAccount::deposit(amt);
+    }
+
+    void monthlyProc() {
+      if (numWithdrawsThisMonth > 4) {
+        int addedFee = numWithdrawsThisMonth - 4;
+        monthlyServiceCharges += addedFee;
+      }
+
+      BankAccount::monthlyProc();
+
+      if (balance < 25) {
+        status = false;
+      }
+    }
+};
