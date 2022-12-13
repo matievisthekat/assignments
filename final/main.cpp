6f078ce7bfdb1f2a784729dc42121f1e1a2cc88e
matievisthekat <matievisthekat@gmail.com>
2022-12-13 09:54:08 +0200

    declare different variables for the classes

---
+++ final/main.cpp
@@ -0,0 +1,10 @@
+#include "CheckingAccount.h"
+#include "SavingsAccount.h"
+
+int main() {
+  SavingsAccount save = SavingsAccount(23.54, 6.99);
+  CheckingAccount check = CheckingAccount(50.67, 9.32);
+
+
+  return 0;
+}
